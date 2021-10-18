c446e655c76eeef40b161eac63a0afb6aaf0fa4f
LauraAssis <laurinha449@gmail.com>
2021-10-18 15:33:41 -0300

    Fazer o commit

---
+++ exemplos.js
@@ -0,0 +1,62 @@
+var firstName = "João";
+var lastName = "Souza"
+
+if(firstName === 'João');{
+    var firstName = 'Pedro';
+    let lastName = 'Silva;'
+}
+
+console.log(firstName, lastName);
+
+
+
+
+const FIRST_NAME = "Bruna";
+
+console.log(FIRST_NAME);
+
+
+
+let um = "um";
+typeof um;
+
+
+let nome = "João";
+let sobrenome = "Pedro";
+
+nome.concat(sobrenome)
+let concatenado = nome.concat(sobrenome)
+
+
+
+let objeto = new String("blablabla")
+typeof objeto;
+exemplo.length;
+String {"blablabla"};
+
+
+
+let vazio = "";
+typeof vazio;
+
+
+
+concatenado = nome + " " + sobrenome;
+concatenado = nome + "\n" + sobrenome;
+concatenado = nome + "\n\n" + sobrenome;
+concatenado = `${nome} ${sobrenome}`;
+concatenado = `${nome} ;
+${sobrenome}`;
+
+concatenando = "\"";
+
+let frase = "Olá, tudo bem?";
+frase.split("");
+frase.split(" ");
+
+frase.includes("tudo")
+
+frase.startsWith("O");
+frase.endsWith("?");
+frase.replace(",", "!");
+let stringModificada = frase.replace(",", "!");
